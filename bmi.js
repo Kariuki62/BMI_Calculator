4b86f46914eecd5ee369163feddaa09204f785e6
Kariuki62 <kariukiwan62@gmail.com>
2020-06-08 15:26:54 -0400

    This is the very first index and bmi js file for this project

---
+++ bmi.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var BMI = weight/(height*height);
+
+res.send("Your BMI result is " + BMI);
+});
+
+
+app.listen(3000,function(){
+console.log("Server has started");
+});
